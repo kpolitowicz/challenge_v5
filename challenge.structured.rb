7fe8a5ca77974470e32031b4b7bf34a799c93719
Kamil Politowicz <kamilp@hey.com>
2024-10-22 15:38:12 -0400

    cleanup: bare minumum structured file

--- challenge.structured.rb
+++ challenge.structured.rb
@@ -2,8 +2,6 @@
 
 require_relative "input_parser"
 
-InputParser.read_and_parse("./user.json")
 users = InputParser.read_and_parse("./users.json")
-InputParser.read_and_parse("./companies.json")
+companies = InputParser.read_and_parse("./companies.json")
 
-puts users
