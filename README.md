1271e51ee9396ba53fa349cb5d180d219f9cae49
Kamil Politowicz <kamilp@hey.com>
2024-10-22 14:15:48 -0400

    feat: actually, let's start with quick-and-dirty

--- README.md
+++ README.md
@@ -4,6 +4,11 @@
 
 1. is exception/exit from the script good enough to handle File.read errors?
 1. is exception/exit from the script good enough to handle JSON.parse errors?
+1. example_output.txt contains sometimes tabs, sometimes spaces at the beginning of lines. Should I just output spaces
+   (or tabs) only or adhere exactly to the format given?
+1. does output file only contain active users (the ones who received a top-up)?
+1. what kind of other errors shpould the script take into acoount (e.g. data missing in fields? mismatched types). What
+   should be the actual handling: is an exception okay or should there be a relevant error message?
 
 ## Assumptions
 
--- challenge.rb
+++ challenge.rb
@@ -1,9 +1,42 @@
 #! ruby
 
-require_relative "input_parser"
+require "json"
 
-users = InputParser.read_and_parse("./user.json")
-users = InputParser.read_and_parse("./users.json")
-companies = InputParser.read_and_parse("./companies.json")
+def group_and_sort_users(company, users)
+  users
+    .select { |u| u["company_id"] == company["id"] && u["active_status"] }
+    .sort_by { |u| u["last_name"] }
+    .group_by { |u| company["email_status"] && u["email_status"] }
+    .values_at(true, false)
+    .map { |list_or_nil| Array(list_or_nil) }
+end
 
-puts users
+def print_user_info(users, company)
+  users.each do |u|
+    puts "\t\t#{u["last_name"]}, #{u["first_name"]}, #{u["email"]}"
+    puts "\t\t  Previous Token Balance, #{u["tokens"]}"
+    puts "\t\t  New Token Balance #{u["tokens"] + company["top_up"]}"
+  end
+end
+
+users = JSON.parse(File.read("./users.json"))
+companies = JSON.parse(File.read("./companies.json"))
+
+companies.sort_by { |c| c["id"] }.each do |company|
+  users_emailed, users_not_emailed = group_and_sort_users(company, users)
+  next if users_emailed.empty? && users_not_emailed.empty?
+
+  puts # the example_output.txt starts with empty line
+  puts "\tCompany Id: #{company["id"]}"
+  puts "\tCompany Name: #{company["name"]}"
+
+  puts "\tUsers Emailed:"
+  print_user_info(users_emailed, company)
+
+  puts "\tUsers Not Emailed:"
+  print_user_info(users_not_emailed, company)
+
+  puts "\t\tTotal amount of top ups for "\
+    "#{company["name"]}: #{(users_emailed.count + users_not_emailed.count) * company["top_up"]}"
+end
+puts # the example_output.txt ends with empty line
