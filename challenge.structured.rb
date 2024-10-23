dbab48385b4c957b85365ad4f8940788217bd7b6
Kamil Politowicz <kamilp@hey.com>
2024-10-23 07:40:12 -0400

    feat: added simple error reporting

--- challenge.structured.rb
+++ challenge.structured.rb
@@ -3,9 +3,14 @@
 require_relative "input_parser"
 require_relative "output_formatter"
 
-input_parser = InputParser.new
-users = input_parser.parse_json("./users.json")
-companies = input_parser.parse_json("./companies.json")
+begin
+  input_parser = InputParser.new
+  users = input_parser.parse_json("./users.json")
+  companies = input_parser.parse_json("./companies.json")
 
-# Using print to output top_up_info as-is (puts would add a new line)
-print OutputFormatter.new(companies, users).top_up_info
+  # Using print to output top_up_info as-is (puts would add a new line)
+  print OutputFormatter.new(companies, users).top_up_info
+rescue InputParser::Error => e
+  warn "There was somthing wrong with input file"
+  warn e.message
+end
